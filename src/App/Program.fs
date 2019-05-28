dff67b8d6fb4dfd36eabb269b39173f6178fc647
nekikara <nekikara@gmail.com>
2019-05-29 00:34:14 +0200

    Say hello

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,8 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open Library
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
     0 // return an integer exit code
