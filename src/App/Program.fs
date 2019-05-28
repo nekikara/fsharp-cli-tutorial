872133c69bab4979301327daf80682cde2857c47
nekikara <nekikara@gmail.com>
2019-05-29 00:47:33 +0200

    Add a function

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,9 +3,5 @@ open Library
 
 [<EntryPoint>]
 let main argv =
-    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
-
-    argv
-    |> Array.map getJsonNetJson
-    |> Array.iter (printfn "%s")
+    toPostfixNotation argv
     0 // return an integer exit code
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -4,3 +4,7 @@ open Newtonsoft.Json
 
 let getJsonNetJson value =
     sprintf "I used to be %s but now I'm %s thanks to JSON.NET!" value (JsonConvert.SerializeObject(value))
+
+let toPostfixNotation expression =
+    expression
+    |> Array.iter (printfn "%s")
